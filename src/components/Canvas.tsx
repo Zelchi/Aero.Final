f1a0cf231e754c75d4ab3fc84357034b5f51b674
victorguimaraesdev <victorguimaraesmax@gmail.com>
2025-02-15 15:41:50 -0300

    Alterações nas formas do Canvas

--- src/components/Canvas.tsx
+++ src/components/Canvas.tsx
@@ -2,9 +2,8 @@ import { useRef, useEffect } from "react";
 import styled from "styled-components";
 
 const Tela = styled.canvas`
-  height: 100%;
-  width: 100%;
-  border: 2px solid white;
+  border: 2px solid rgba(248, 248, 248, 0.5);
+  border-radius: 10px;
 `;
 
 const Canvas = () => {
@@ -15,18 +14,16 @@ const Canvas = () => {
     if (!canvas) return;
     const context = canvas.getContext("2d");
     if (!context) return;
-    context.fillStyle = "#2e0303";
-    context.fillRect(0, 0, context.canvas.width, context.canvas.height);
-    context.fillStyle = "rgb(200,0,0)";
-    context.fillRect(10, 10, 55, 50);
-    context.fillStyle = "rgba(248, 248, 248, 0.5)";
-    // context.fillRect(30, 30, 55, 50);
-    context.fillRect(25, 25, 100, 100);
-    context.clearRect(45, 45, 60, 60);
-    context.strokeRect(50, 50, 50, 50);
+    context.scale(1, 1);
+    context.fillStyle = "#000000";
+    context.fillRect(0, 0, 600, 600);
+    context.fillStyle = "rgb(119, 6, 5)";
+    context.fillRect(150, 450, 70, 10);
+    context.fillStyle = "rgb(119, 6, 5)";
+    context.fillRect(400, 450, 70, 10);
   }, []);
 
-  return <Tela ref={canvasRef} />;
+  return <Tela ref={canvasRef} width="600" height="600" />;
 };
 
 export default Canvas;
--- src/components/global/BarraJanela.tsx
+++ src/components/global/BarraJanela.tsx
@@ -1,79 +1,89 @@
-import { useState } from 'react'
-import styled from 'styled-components'
+import { useState } from "react";
+import styled from "styled-components";
 
 const Barra = styled.div`
-    width: 100vw;
-    display: flex;
-    flex-direction: row;
-    justify-content: space-between;
-    align-items: center;
-    background-color: #3c3c3c;
-    color: white;
-    -webkit-app-region: drag;
-`
+  width: 100vw;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  background-color: #3c3c3c;
+  color: white;
+  -webkit-app-region: drag;
+  border-radius: 20px;
+`;
 
 const Titulo = styled.h1`
-    font-size: 16px;
-`
+  font-size: 16px;
+`;
 
 const Caixa1 = styled.div`
-    display: flex;
-    justify-content: start;
-    align-items: center;
-    width: 100px;
-`
+  display: flex;
+  justify-content: start;
+  align-items: center;
+  width: 100px;
+`;
 
 const Caixa2 = styled.div`
-    display: flex;
-    justify-content: end;
-    align-items: center;
-    width: 100px;
-    gap: 5px;
-`
+  display: flex;
+  justify-content: end;
+  align-items: center;
+  width: 100px;
+  gap: 5px;
+`;
 
 const Minimizar = styled.div<{ $minicolor: boolean }>`
-    -webkit-app-region: no-drag;
-    width: 20px;
-    height: 20px;
-    border: 2px inset black;
-    cursor: pointer;
-    background-color: ${({ $minicolor }) => ($minicolor ? '#7a007a' : '#4e004e')};
-`
+  -webkit-app-region: no-drag;
+  width: 20px;
+  height: 20px;
+  border: 2px inset black;
+  cursor: pointer;
+  background-color: ${({ $minicolor }) => ($minicolor ? "#7a007a" : "#4e004e")};
+`;
 
 const Fechar = styled.div`
-    -webkit-app-region: no-drag;
-    width: 20px;
-    height: 20px;
-    margin-right: 10px;
-    border: 2px inset black;
-    background-color: #00026e;
+  -webkit-app-region: no-drag;
+  width: 20px;
+  height: 20px;
+  margin-right: 10px;
+  border: 2px inset black;
+  background-color: #00026e;
 
-    &:hover {
-        cursor: pointer;
-        background-color: #0e003f;
-    }
-`
+  &:hover {
+    cursor: pointer;
+    background-color: #0e003f;
+  }
+`;
 
 export const BarraJanela = () => {
-    const [miniColor, setMiniColor] = useState(false);
+  const [miniColor, setMiniColor] = useState(false);
 
-    return (
-        <Barra>
-            <Caixa1>
-                
-            </Caixa1>
+  return (
+    <Barra>
+      <Caixa1></Caixa1>
 
-            <Titulo>Batata Voadora</Titulo>
+      <Titulo>Batata Voadora</Titulo>
 
-            <Caixa2>
-                <Minimizar
-                    onMouseMove={() => { setMiniColor(true) }}
-                    onMouseOut={() => { setMiniColor(false) }}
-                    onClick={() => { window.api('minimizar'); setMiniColor(false) }}
-                    $minicolor={miniColor}
-                />
-                <Fechar onClick={() => { window.api('fechar'); }} />
-            </Caixa2>
-        </Barra>
-    )
-}
+      <Caixa2>
+        <Minimizar
+          onMouseMove={() => {
+            setMiniColor(true);
+          }}
+          onMouseOut={() => {
+            setMiniColor(false);
+          }}
+          onClick={() => {
+            window.api("minimizar");
+            setMiniColor(false);
+          }}
+          $minicolor={miniColor}
+        />
+        <Fechar
+          onClick={() => {
+            window.api("fechar");
+          }}
+        />
+      </Caixa2>
+    </Barra>
+  );
+};
