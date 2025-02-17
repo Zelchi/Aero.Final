5f66cfef66376b53cf50b9337bb9c0ded49570c8
victorguimaraesdev <victorguimaraesmax@gmail.com>
2025-02-17 15:06:01 -0300

    Adicionado Constantes

--- src/components/Canvas.tsx
+++ src/components/Canvas.tsx
@@ -14,16 +14,7 @@ const Canvas = () => {
     if (!canvas) return;
     const context = canvas.getContext("2d");
     if (!context) return;
-    // //background
-    // context.fillStyle = "#000000";
-    // context.fillRect(0, 0, 600, 600);
-    // //barreira 1
-    // context.fillStyle = "rgb(119, 6, 5)";
-    // context.fillRect(150, 450, 60, 15);
-    // //barreira 2
-    // context.fillStyle = "rgb(119, 6, 5)";
-    // context.fillRect(400, 450, 60, 15);
-    // //player
+
     const player = new Player(canvas.width, canvas.height);
     const keys = {
       left: false,
