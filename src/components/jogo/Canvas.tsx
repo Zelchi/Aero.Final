0475986ccec34645d8ccb878c31c1a608a393c3f
victorguimaraesdev <victorguimaraesmax@gmail.com>
2025-02-18 14:38:45 -0300

    Refactor

--- src/components/jogo/Canvas.tsx
+++ src/components/jogo/Canvas.tsx
@@ -1,59 +1,43 @@
-import { useRef, useEffect } from "react";
+import { useEffect, useRef, useState } from "react";
 import Player from "../../classes/Player";
+import gameLoop from "./scripts/GameLoop";
+
+const keys = {
+  left: false,
+  right: false,
+  up: false,
+  down: false,
+};
+
+addEventListener("keydown", (e) => {
+  const key = e.key.toLowerCase();
+  if (key === "a") keys.left = true;
+  if (key === "d") keys.right = true;
+  if (key === "w") keys.up = true;
+  if (key === "s") keys.down = true;
+});
+addEventListener("keyup", (e) => {
+  const key = e.key.toLowerCase();
+  if (key === "a") keys.left = false;
+  if (key === "d") keys.right = false;
+  if (key === "w") keys.up = false;
+  if (key === "s") keys.down = false;
+});
 
 const Canvas = ({ $largura, $altura }: tamanhoTela) => {
-    const canvasRef = useRef<HTMLCanvasElement>(null);
+  const [player] = useState(new Player($largura, $altura));
+  const canvasRef = useRef<HTMLCanvasElement>(null);
 
-    useEffect(() => {
-        const canvas = canvasRef.current;
-        if (!canvas) return;
-        const context = canvas.getContext("2d");
-        if (!context) return;
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    if (!canvas) return;
+    const context = canvas.getContext("2d");
+    if (!context) return;
 
-        const player = new Player(canvas.width, canvas.height);
-        const keys = {
-            left: false,
-            right: false,
-            up: false,
-            down: false,
-        };
-        const gameLoop = () => {
-            context.clearRect(0, 0, canvas.width, canvas.height);
-            if (keys.left && player.position.x >= 0) {
-                player.moveLeft();
-            }
-            if (keys.right && player.position.x < canvas.width - player.width - 10) {
-                player.moveRight();
-            }
-            if (keys.up && player.position.y >= 0) {
-                player.moveUp();
-            }
-            if (keys.down && player.position.y < canvas.height - player.height - 10) {
-                player.moveDown();
-            }
-            player.draw(context);
-            requestAnimationFrame(gameLoop);
-        };
+    gameLoop(canvas, context, keys, player);
+  }, [player, canvasRef]);
 
-        addEventListener("keydown", (e) => {
-            const key = e.key.toLowerCase();
-
-            if (key === "a") keys.left = true;
-            if (key === "d") keys.right = true;
-            if (key === "w") keys.up = true;
-            if (key === "s") keys.down = true;
-        });
-        addEventListener("keyup", (e) => {
-            const key = e.key.toLowerCase();
-            if (key === "a") keys.left = false;
-            if (key === "d") keys.right = false;
-            if (key === "w") keys.up = false;
-            if (key === "s") keys.down = false;
-        });
-        gameLoop();
-    }, []);
-
-    return <canvas ref={canvasRef} width={$largura} height={$altura} />;
+  return <canvas ref={canvasRef} width={$largura} height={$altura} />;
 };
 
 export default Canvas;
---
+++ src/components/jogo/scripts/GameLoop.ts
@@ -0,0 +1,33 @@
+import Player from "../../../classes/Player";
+type Keys = {
+  left: boolean;
+  right: boolean;
+  up: boolean;
+  down: boolean;
+};
+const gameLoop = (
+  canvas: HTMLCanvasElement,
+  context: CanvasRenderingContext2D,
+  keys: Keys,
+  player: Player
+) => {
+  function draw() {
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    if (keys.left && player.position.x >= 0) {
+      player.moveLeft();
+    }
+    if (keys.right && player.position.x < canvas.width - player.width - 10) {
+      player.moveRight();
+    }
+    if (keys.up && player.position.y >= 0) {
+      player.moveUp();
+    }
+    if (keys.down && player.position.y < canvas.height - player.height - 10) {
+      player.moveDown();
+    }
+    player.draw(context);
+    requestAnimationFrame(draw);
+  }
+  requestAnimationFrame(draw);
+};
+export default gameLoop;
