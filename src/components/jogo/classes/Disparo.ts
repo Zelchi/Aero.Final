8163dd37c11836f22a6fbd7b7b9f8eb8684f05b3
victorguimaraesdev <victorguimaraesmax@gmail.com>
2025-02-22 18:08:12 -0300

    A bizarrice das colisões

--- src/components/jogo/classes/Disparo.ts
+++ src/components/jogo/classes/Disparo.ts
@@ -1,15 +1,19 @@
+import { Rock } from "./Rock";
+
 export class Disparo {
   position: { x: number; y: number };
   velocity: number;
   largura: number;
   altura: number;
   angulo: number;
+  verificarColisao: boolean;
   constructor(position: { x: number; y: number }, angulo: number) {
     this.position = position;
     this.largura = 10;
     this.altura = 10;
     this.velocity = 10;
     this.angulo = angulo;
+    this.verificarColisao = false;
   }
   draw(context: CanvasRenderingContext2D) {
     context.save();
@@ -31,4 +35,14 @@ export class Disparo {
     this.position.x += Math.cos(this.angulo) * this.velocity;
     this.position.y += Math.sin(this.angulo) * this.velocity;
   }
+  colisao = (rock: Rock) => {
+    if (
+      rock.position.x + rock.largura >= this.position.x &&
+      rock.position.x <= this.position.x + this.largura &&
+      rock.position.y + rock.altura >= this.position.y &&
+      rock.position.y <= this.position.y + this.largura
+    ) {
+      this.verificarColisao = true;
+    }
+  };
 }
--- src/components/jogo/classes/Jogo.ts
+++ src/components/jogo/classes/Jogo.ts
@@ -7,101 +7,122 @@ import { Crosshair } from "./Crosshair";
 // Todas as entidades do jogo são renderizadas aqui.
 // Estão encapsuladas no objeto jogo.
 export class Jogo {
-    context: CanvasRenderingContext2D;
-    player: Player;
-    crosshair: Crosshair;
-    rocks: Rock[];
-    disparos: Disparo[];
+  context: CanvasRenderingContext2D;
+  player: Player;
+  crosshair: Crosshair;
+  rocks: Rock[];
+  disparos: Disparo[];
 
-    constructor(context: CanvasRenderingContext2D) {
-        this.context = context;
-        this.player = new Player(context.canvas.width, context.canvas.height);
-        this.crosshair = new Crosshair(context, { x: context.canvas.width / 2, y: context.canvas.height / 2 });
-        this.rocks = [];
-        this.disparos = [];
-    }
+  constructor(context: CanvasRenderingContext2D) {
+    this.context = context;
+    this.player = new Player(context.canvas.width, context.canvas.height);
+    this.crosshair = new Crosshair(context, {
+      x: context.canvas.width / 2,
+      y: context.canvas.height / 2,
+    });
+    this.rocks = [];
+    this.disparos = [];
+  }
 
-    renderizarJogo = () => {
-        // Loop do jogo
-        this.inputsJogador();
-        this.gerarPedras(this.context, 2000);
+  renderizarJogo = () => {
+    // Loop do jogo
+    this.inputsJogador();
+    this.gerarPedras(this.context, 2000);
 
-        // Desenha o jogo
-        const draw = () => { //Pegar o o tempo e colocar como o modificador de velocidade
-            this.context.imageSmoothingEnabled = false;
-            this.context.clearRect(0, 0, this.context.canvas.width, this.context.canvas.height); // Limpa para o novo frame
-            this.context.save(); // Salvando o estado do contexto
+    // Desenha o jogo
+    const draw = () => {
+      //Pegar o o tempo e colocar como o modificador de velocidade
+      this.context.imageSmoothingEnabled = false;
+      this.context.clearRect(
+        0,
+        0,
+        this.context.canvas.width,
+        this.context.canvas.height
+      ); // Limpa para o novo frame
+      this.context.save(); // Salvando o estado do contexto
 
-            // Renderiza o array de pedras
-            this.rocks.forEach((rock, index) => {
-                rock.renderizar(rock, this.context);
-                if (rock.position.y > this.context.canvas.height) {
-                    this.rocks.splice(index, 1);
-                }
-            });
+      //verifica colisão
+      this.rocks.forEach((rock) => {
+        this.disparos.forEach((disparo) => {
+          rock.colisao(disparo);
+          disparo.colisao(rock);
+        });
+      });
 
-            // Renderiza o array de disparos
-            this.disparos.forEach((disparo, index) => {
-                disparo.renderizar(this.context);
-                if (disparo.position.y > this.context.canvas.height) {
-                    this.disparos.splice(index, 1);
-                }
-            });
+      this.rocks = this.rocks.filter((rock) => !rock.verificarColisao);
+      this.disparos = this.disparos.filter(
+        (disparo) => !disparo.verificarColisao
+      );
 
-            // Renderiza o crosshair
-            this.crosshair.renderizar();
-            // Renderiza o player
-            this.player.ajustarAngulo({ x: this.crosshair.x, y: this.crosshair.y });
-            this.player.renderizar(this.player, this.context);
-            this.context.restore(); // Restaurando o estado do contexto
-            requestAnimationFrame(draw);
+      // Renderiza o array de pedras
+      this.rocks.forEach((rock, index) => {
+        rock.renderizar(rock, this.context);
+        if (rock.position.y > this.context.canvas.height) {
+          this.rocks.splice(index, 1);
         }
-        requestAnimationFrame(draw);
-    }
+      });
 
-    // Inputs do jogador
-    inputsJogador = () => {
-        // Evento de mover o mouse
-        document.addEventListener('mousemove', (e) => {
-            this.crosshair.update({ x: e.clientX, y: e.clientY });
-        });
+      // Renderiza o array de disparos
+      this.disparos.forEach((disparo, index) => {
+        disparo.renderizar(this.context);
+        if (disparo.position.y > this.context.canvas.height) {
+          this.disparos.splice(index, 1);
+        }
+      });
 
-        // Evento de pressionar clique do mouse
-        document.addEventListener('click', () => {
-            this.gerarDisparos();
-        })
-
-        // Evento de pressionar a tecla
-        document.addEventListener('keydown', (e) => {
-            const key = e.key.toLowerCase();
-            this.player.keydown(key);
-
-        });
-
-        // Evento de soltar a tecla
-        document.addEventListener('keyup', (e) => {
-            const key = e.key.toLowerCase();
-            this.player.keyup(key);
-        });
-    }
-
-    // Gera os disparos na tela
-    gerarDisparos = () => {
-        const novoDisparo = new Disparo(
-            {
-                x: this.player.position.x + this.player.largura / 2,
-                y: this.player.position.y + this.player.altura / 2,
-            },
-            this.player.angulo - Math.PI / 2
-        );
-        this.disparos.push(novoDisparo);
+      // Renderiza o crosshair
+      this.crosshair.renderizar();
+      // Renderiza o player
+      this.player.ajustarAngulo({ x: this.crosshair.x, y: this.crosshair.y });
+      this.player.renderizar(this.player, this.context);
+      this.context.restore(); // Restaurando o estado do contexto
+      requestAnimationFrame(draw);
     };
+    requestAnimationFrame(draw);
+  };
 
-    // Gera as pedras a cada (2000) = 2 segundos.
-    gerarPedras = (context: CanvasRenderingContext2D, intervalo: number) => {
-        setInterval(() => {
-            const novaPedra = new Rock(context.canvas.width, context.canvas.height);
-            this.rocks.push(novaPedra);
-        }, intervalo);
-    }
+  // Inputs do jogador
+  inputsJogador = () => {
+    // Evento de mover o mouse
+    document.addEventListener("mousemove", (e) => {
+      this.crosshair.update({ x: e.clientX, y: e.clientY });
+    });
+
+    // Evento de pressionar clique do mouse
+    document.addEventListener("click", () => {
+      this.gerarDisparos();
+    });
+
+    // Evento de pressionar a tecla
+    document.addEventListener("keydown", (e) => {
+      const key = e.key.toLowerCase();
+      this.player.keydown(key);
+    });
+
+    // Evento de soltar a tecla
+    document.addEventListener("keyup", (e) => {
+      const key = e.key.toLowerCase();
+      this.player.keyup(key);
+    });
+  };
+
+  // Gera os disparos na tela
+  gerarDisparos = () => {
+    const novoDisparo = new Disparo(
+      {
+        x: this.player.position.x + this.player.largura / 2,
+        y: this.player.position.y + this.player.altura / 2,
+      },
+      this.player.angulo - Math.PI / 2
+    );
+    this.disparos.push(novoDisparo);
+  };
+
+  // Gera as pedras a cada (2000) = 2 segundos.
+  gerarPedras = (context: CanvasRenderingContext2D, intervalo: number) => {
+    setInterval(() => {
+      const novaPedra = new Rock(context.canvas.width, context.canvas.height);
+      this.rocks.push(novaPedra);
+    }, intervalo);
+  };
 }
